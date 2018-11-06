fab0b3ce6e42c8d47500285fc02b5b3b8ed7e697
Dennis Gannon <dennisg798@gmail.com>
2018-11-06 13:13:13 -0500

    Finished buttons with JavaScript and HTML

---
+++ functions.js
@@ -0,0 +1,22 @@
+//function consoleHello(){
+//  var p=document.getElementById("hello");
+//  p.innerHTML="Hello, World!";
+//}
+
+function consoleHello() {
+  console.log("Hello, World")
+}
+
+function alertHello() {
+  alert("Hello, World!");
+}
+
+function browserHello() {
+  var p=document.getElementById("browser");
+  p.innerHTML="Hello, World!";
+}
+
+function strangerHello() {
+  var stranger=prompt("Who are you?");
+  alert("Hello, " + stranger);
+}
